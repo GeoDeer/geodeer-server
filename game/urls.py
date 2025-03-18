8a286f8de5024af2634c37ed47e634fa8ddfcf12
halukhndstn <145353400+halukhndstn@users.noreply.github.com>
2025-03-18 23:09:36 +0300

    template and urls updates

--- game/urls.py
+++ game/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('monitor/', views.monitor, name='monitor'),   
 ]
