aee174934ded8dd2886c3a5e10bc8adebf3e72d0
halukhndstn <145353400+halukhndstn@users.noreply.github.com>
2025-04-01 22:07:09 +0300

    Update admin.py

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User, Game, Waypoint, UserScore, UserLocation, UserDistance
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Game)
+admin.site.register(Waypoint)
+admin.site.register(UserScore)
+admin.site.register(UserLocation)
+admin.site.register(UserDistance)
