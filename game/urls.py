ce0c30da7020898dcc36c962d778a169c6928f95
halukhndstn <145353400+halukhndstn@users.noreply.github.com>
2025-04-13 00:16:15 +0300

    create-manage game template

--- game/urls.py
+++ game/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('main-menu/<int:creator_id>/', views.main_menu, name='main_menu'),
+    path('main-menu/<int:creator_id>', views.main_menu, name='main_menu'),
     path('create-manage/<int:creator_id>/', views.create_manage, name='create_manage'),
     path('monitor/<int:pk>/<int:creator_id>/', views.monitor, name='monitor'), 
 ]
